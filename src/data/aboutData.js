6ee0a9bdb82cd84bc5810199e2e7f8c3bf0e1df6
Marko Tomik <57318370+mvrque@users.noreply.github.com>
2024-12-05 19:58:11 +0200

    Update aboutData.js

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,7 +1,8 @@
 export const aboutData = {
     title: "Your next software engineer!",
     description1: "Hello! I'm Marko and i'm ready to change the world one line of code at a time.",
-    description2: "I am working in a manufacturing industry producing high quality e-bikes. Working in a sustainable factory with a wide range of different tools and technologies. Having intense workflows and collaboration with other team members has secured satisfaction in our customers. It is good to know that one bike makes a huge impact towards our climate! Using different technologies sparked an interest in building my own software to make the world a better place! I took it upon myself to learn Javascript - from design to testing and documentation. While producing quality bikes i am also leaning into my engineering skillset to work with different clients and to freelance. I am ready to join a product team to build software that people love!",
+    description2: "Currently I am working in Olympic Entertainment Group as a technician in a IT department. My job consist fixing and servicing different machines that are in the casinos. Using different technologies sparked an interest in building my own software to make the world a better place! I took it upon myself to learn Javascript - from design to testing and documentation. I also love fixing and riding bikes so right now I am working on a project that is going to help the bike world! I am also leaning into my sofware engineering skillset to work with clients and to freelance. I am ready to join a product team to build software that people love!",
+    
 
     image: 1
 }
