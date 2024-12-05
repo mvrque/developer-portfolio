cde3650b736baae8d92ccbc9fd6b418b4eac402f
Marko Tomik <57318370+mvrque@users.noreply.github.com>
2024-12-05 20:00:04 +0200

    Update experienceData.js

--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -1,17 +1,24 @@
 export const experienceData = [
     {
         id: 1,
+        company: 'Olympic Entertainment Group',
+        jobtitle: 'Machine Technician',
+        startYear: '2024 September',
+        endYear: 'present'
+    },
+    {
+        id: 2,
         company: '100Devs',
         jobtitle: 'Software Engineer',
         startYear: '2022 January',
         endYear: 'present'
     },
     {
-        id: 2,
+        id: 3,
         company: 'Ampler Bikes',
         jobtitle: 'Assembly Technician',
         startYear: '2021 October',
-        endYear: 'present'
+        endYear: '2023'
     },
     
 ]
