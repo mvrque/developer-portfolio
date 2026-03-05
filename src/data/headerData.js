2843f998297fce54d105cc4350efd51c180f3cb0
Marko Tomik <57318370+mvrque@users.noreply.github.com>
2026-03-05 11:39:00 +0200

    Update headerData.js

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -2,8 +2,8 @@ import resume from '../assets/pdf/resume.pdf'
 
 export const headerData = {
     name: 'Marko Tomik',
-    title: "Software Engineer",
-    desciption:"Located in Tallinn, Estonia ",
+    title: "Aviation Engineer Student",
+    desciption:"Located in Tartu, Estonia ",
     image: 'https://pbs.twimg.com/profile_images/1515070462199570439/srnJxngo_400x400.jpg'
     
 }
