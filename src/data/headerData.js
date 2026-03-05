09d3692dfc08710c4eefe165551b35a1868cd971
Marko Tomik <57318370+mvrque@users.noreply.github.com>
2026-03-05 11:09:25 +0200

    Update headerData.js

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -2,8 +2,8 @@ import resume from '../assets/pdf/resume.pdf'
 
 export const headerData = {
     name: 'Marko Tomik',
-    title: "Junior Web Developer",
-    desciption:"Located in Tallinn, Estonia ",
+    title: "Student at Estonian Aviation Academy",
+    desciption:"Located in Tartu, Estonia ",
     image: 'https://i.ibb.co/bWvbkLT/IMG-4378.jpg',
     
 }
