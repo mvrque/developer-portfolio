58d9a8f26459e547a02b1c5bd03744832a7d3e18
Marko Tomik <57318370+mvrque@users.noreply.github.com>
2026-03-05 11:35:36 +0200

    Update aboutData.js

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,7 +1,7 @@
 export const aboutData = {
     title: "Your next software engineer!",
     description1: "Hello! I'm Marko and i'm ready to change the world one line of code at a time.",
-    description2: "Currently I am working in Olympic Entertainment Group as a technician in a IT department. My job consist fixing and servicing different machines that are in the casinos. Using different technologies sparked an interest in building my own software to make the world a better place! I took it upon myself to learn Javascript - from design to testing and documentation. I also love fixing and riding bikes so right now I am working on a project that is going to help the bike world! I am also leaning into my sofware engineering skillset to work with clients and to freelance. I am ready to join a product team to build software that people love!",
+    description2: "I am a first-year Aviation Engineering student at the Estonian Aviation Academy with an interest in communication and telecommunication systems. Alongside my studies, I enjoy programming and have previously learned full-stack web development independently, as well as Python during my studies.Before starting my studies, I worked for several years as a bicycle mechanic and later as a gaming equipment technician at Olympic Casino, where I was responsible for maintaining and troubleshooting technical systems.",
     
 
     image: 1
